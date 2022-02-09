538ba86a00e14db0dc9f335115461def89ce7a5a
Eletie <60183758+Eletie@users.noreply.github.com>
2022-02-09 13:16:28 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@lab/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  string a;
+  cout << "Vārds\n";
+  cin >> a;
+  if (a == "es") {
+    cout << "nē es!";
+  }
+  cout << "Ievadītais vārds " + a;
+}
